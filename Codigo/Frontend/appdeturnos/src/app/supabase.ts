2c10dbdde756f260f4060d70dfb23028507fe784
Maria <mariapazcgh0@gmail.com>
2025-10-06 22:26:52 -0300

    1

--- Codigo/Frontend/appdeturnos/src/app/supabase.ts
+++ Codigo/Frontend/appdeturnos/src/app/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rvkimjcnwvfjelnnzsnb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2a2ltamNud3ZmamVsbm56c25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNzExNDgsImV4cCI6MjA3Mjk0NzE0OH0.7_FlcBCI5vQhbR-MvJNlaZEkzjN6T7NeHMzccZL0lq4';
+const supabaseUrl = 'https://laykevvzjskyiqsiwltc.supabase.co';
+
+';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxheWtldnZ6anNreWlxc2l3bHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyODE1NzIsImV4cCI6MjA2ODg1NzU3Mn0.RsF0YXsgrnDayeknoQ5fOGQV42sjV-2n83SrVp0A2lU';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
