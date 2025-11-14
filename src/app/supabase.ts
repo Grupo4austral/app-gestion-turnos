aac9b47f0c2e46be6adb43a045943099747834c8
Gustavo Frers <taiofrers@gmail.com>
2025-11-14 19:23:13 -0300

    q

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import { environment } from '../environments/environment';
 
 export const supabase = createClient(
-  environment.supabase.url,
-  environment.supabase.anonKey
+  'https://laykevvzjskyiqsiwltc.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxheWtldnZ6anNreWlxc2l3bHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyODE1NzIsImV4cCI6MjA2ODg1NzU3Mn0.RsF0YXsgrnDayeknoQ5fOGQV42sjV-2n83SrVp0A2lU'
 );
