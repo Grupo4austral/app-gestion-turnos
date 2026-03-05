c1cc96a9b63693cd442557a2c803f8492e038842
Taio <taiofrers@gmail.com>
2026-03-05 13:58:50 -0300

    s

---
+++ src/ionicons-icons.d.ts
@@ -0,0 +1,7 @@
+declare module 'ionicons/icons' {
+  export const addCircleOutline: string;
+  export const saveOutline: string;
+  export const closeOutline: string;
+  export const createOutline: string;
+  export const trashOutline: string;
+}
